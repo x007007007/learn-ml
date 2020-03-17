a9ec19e5e48eec1d80418413a4af486c97ab9c79
xxc <x007007007@hotmail.com>
2020-03-17 22:35:51 +0800

    add script install requirement

--- initial-env.py
+++ initial-env.py
@@ -1,9 +1,18 @@
 import os
 import subprocess
+import platform
+
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-VENV_PATH = os.path.join(ROOT_PATH, "venv")
+VENV_PATH = os.path.join(ROOT_PATH, "pytorch", "venv")
+
+PYTORCH_PIP_PATH = os.path.join(ROOT_PATH, "pytorch", "venv", "scripts", "pip.exe")
+PYTORCH_PIP_requirement = os.path.join(ROOT_PATH, "pytorch", "requirements.txt")
+
 if not os.path.exists(VENV_PATH):
     import venv
     venv.create(VENV_PATH, with_pip=True, prompt="pytorch-venv")
+
+install_arguments = [PYTORCH_PIP_PATH, "-r", PYTORCH_PIP_requirement]
+subprocess.call(install_arguments, shell=True)
 
